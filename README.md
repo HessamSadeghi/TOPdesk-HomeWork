ddf79100cd9ba605ea3d84de5375bdebb3fd14bd
HessamSadeghi <panzon.h@gmail.com>
2023-12-15 12:22:10 +0330

    feat(main) css minimizer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ mkdir TOPdesk-HomeWork
 cd TOPdesk-HomeWork
 npm init -y
 npm install express webpack
-npm install -D css-loader html-webpack-plugin mini-css-extract-plugin webpack-cli
+npm install -D css-loader css-minimizer-webpack-plugin html-webpack-plugin mini-css-extract-plugin webpack-cli
 
 npx webpack --config webpack.config.js
 node server/server.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
 	mode: 'production',
@@ -20,6 +21,12 @@ module.exports = {
 					'css-loader',
 				],
 			},
+		],
+	},
+	optimization: {
+		minimizer: [
+			`...`,
+			new CssMinimizerPlugin(),
 		],
 	},
 	plugins: [
